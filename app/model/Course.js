013127e107cb2d4c050f817fea3d61709dc23264
Antti Vuorela <antti.vuorela@iki.fi>
2012-05-20 19:42:06 +0300

    Updated new api path

--- app/model/Course.js
+++ app/model/Course.js
@@ -32,7 +32,7 @@ if (courseLast){
 	CourseStore.each(function(course){
 		var code = course.get("code");
 		console.log("Loading course " + code, course);
-		var url = 'http://verkel.iki.fi:8080/course/'+code+'/all';
+		var url = 'http://mobilenoppa.dy.fi/api/course/'+code+'/all';
 		if (course.get("exerciseGroup")) {
 		  url += "?group=" + course.get("exerciseGroup");
 		}
