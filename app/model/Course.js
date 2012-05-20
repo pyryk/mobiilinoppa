e75e13fab4f2e9961527569fa97144ec7359909c
Antti Vuorela <antti.vuorela@iki.fi>
2012-05-20 22:03:46 +0300

    Persistent todo state during update

--- app/model/Course.js
+++ app/model/Course.js
@@ -12,26 +12,41 @@ Ext.define('MobileNoppa.model.Course', {
   }
 });
 
-// TODO move to some better place (Course model static method?)
+/*
+ * Global function to update the localstorage course store from API
+ */
 window.mobilenoppa.refreshCourseData = function() {
-	console.log("[MobileNoppa.controller.Courses] loadCourseItems");
+	console.log("[MobileNoppa.model.Course] refreshCourseData");
 
 	var CourseStore = Ext.getStore('Courses');
 	var CourseItemStore = Ext.getStore('CourseItems');
-	CourseItemStore.removeAll();
 
+	// Continue only if there are any courses added
 	if (CourseStore.data.length > 0) {
+		
+		// Show Loading mask
 	  Ext.Viewport.setMasked({
 	  	xtype: 'loadmask',
 	  	message: 'Loading course data'
 	  });
+	
+		// Save the status of todo-tasks to a temporary variable
+		var todoStatusTemp = {};
+		CourseItemStore.each(function(courseItem){
+			if(courseItem.get("type") == "assignment"){
+				var hash = courseItem.get("course_id") + courseItem.get("title"); 
+				todoStatusTemp[hash] = courseItem.get("todo_status"); 
 	}
+		});
+		
+		// Remove old items
+		CourseItemStore.removeAll();
 
-	var courseLast = CourseStore.data.last();
-	if (courseLast){
+		// Load items for each course
 		CourseStore.each(function(course){
 			var code = course.get("code");
 			console.log("Loading course " + code, course);
+			
 			var url = window.mobilenoppa.apiUrl + 'course/'+code+'/all';
 			if (course.get("exerciseGroup")) {
 			  url += "?group=" + course.get("exerciseGroup");
@@ -42,18 +57,30 @@ window.mobilenoppa.refreshCourseData = function() {
 				success: function(response){
 					var text = response.responseText;
 					var json = Ext.JSON.decode(text);
-					//console.log(text,json);
 					if (json && json.courseItems){
+						// Loop through courseItems to add them to store
 						for(var i=0;i<json.courseItems.length;i++){
 							var jsonItem = json.courseItems[i];
+							
+							// Set the course id
 							jsonItem["course_id"] = course.getId();
+							
+							// If item type is assignment, check for previous todo state
+							if(jsonItem.type = "assignment"){
+								var hash = jsonItem.course_id + jsonItem.title;
+								if(todoStatusTemp[hash]){
+									console.log("Restoring todo state",jsonItem,todoStatusTemp[hash]);
+									jsonItem["todo_status"] = todoStatusTemp[hash];
+								}
+							}
+							// Add the item to CourseItemStore
 							var courseItem = CourseItemStore.add(jsonItem);
-							//courseItem[0].setCourse(course.getId());
-							//console.log(jsonItem,courseItem,course);
 						}
 					
+						// Save the store to localstorage
 						CourseItemStore.sync();
 					}
+					// Hide the loader spinner
 					Ext.Viewport.setMasked(false);
 				}
 			});
